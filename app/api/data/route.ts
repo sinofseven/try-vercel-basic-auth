d30db12866e2379328ad7b93295a691bad8d058c
sinofseven <em.s.00001@gmail.com>
2023-05-30 20:40:31 +0900

    fix

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
 
 export function GET() {
-  return NextResponse.json({msg: 'test'}, {headers: {'Content-Type': 'application/json'}})
+  return NextResponse.json({msg: 'test'}, {headers: {'content-type': 'application/json'}})
 }
